3152ba9093474c33a0695cfe762086b9acadc9d7
Sha256EO <sha256evakorunn@outlook.com>
2024-12-05 12:36:57 -0300

    se agregan las configuraciones para netflify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Asegúrate de que la base esté configurada correctamente
+  build: {
+    target: 'esnext', // Genera código compatible con módulos modernos
+    manifest: true,
+  },
+});
