541db429c6c37116a3042dda30816eb06a9ee546
Sha256EO <sha256evakorunn@outlook.com>
2024-12-06 14:24:09 -0300

    correccion en el path

--- src/components/Auth/Protected.jsx
+++ src/components/Auth/Protected.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import apiClient from '../services/apiClient'; // Cliente Axios
+import apiClient from '../../services/apiClient';
 
 const Dashboard = () => {
   const [message, setMessage] = useState('');
